1a6986903ebf3307e1a79ce9ae1f28b50fc782f8
Nguyễn Tuấn Anh <tuananhpro2981995@gmail.com>
2016-12-02 01:51:28 +0700

    no message

--- FinalProject/WebAPI/App_Start/WebApiConfig.cs
+++ FinalProject/WebAPI/App_Start/WebApiConfig.cs
@@ -15,8 +15,6 @@ namespace WebAPI
     {
         public static void Register(HttpConfiguration config)
         {
-            var cors = new EnableCorsAttribute("*", "*", "*");
-            config.EnableCors(cors);
             // Web API configuration and services
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
